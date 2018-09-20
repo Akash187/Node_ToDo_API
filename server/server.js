582c4db8b89c93ec58a2e01a3d63214ae2195e4f
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-09-21 00:45:31 +0530

    Test POST /todos route.

--- server/server.js
+++ server/server.js
@@ -20,6 +20,18 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req,res) => {
+  Todo.find().then(() => {
+
+  }, () => {
+
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {
+  app
+};
