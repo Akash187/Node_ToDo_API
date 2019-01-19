4ca9dbd0583f40af14dd2cbd01034ac702c5a57f
Akash Kumar Seth <kumarakashseth007@gmail.com>
2019-01-19 11:35:29 +0530

    Added code to send only _id and email back in response to POST '/users'.

--- server/modals/user.js
+++ server/modals/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 
 // {
@@ -40,6 +41,12 @@ let UserSchema = new mongoose.Schema({
     }
   }]
 });
+
+UserSchema.methods.toJSON = function(){
+  let user = this;
+  let userObject = user.toObject();
+  return _.pick(userObject, ['_id', 'email']);
+};
 
 UserSchema.methods.generateAuthToken = function(){
   let user = this;
